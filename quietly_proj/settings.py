3528020f4ba3707ce15468a5427f8dc882ad37c0
rohituxuistudio <Rohit@uxuistudio.in>
2025-10-23 22:10:05 +0530

    cors fixed

--- quietly_proj/settings.py
+++ quietly_proj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'corsheaders',
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
@@ -68,15 +69,23 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",             # your Vite dev server
+    "https://quietly.tools/",  # your deployed React app
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'quietly_proj.urls'
 
