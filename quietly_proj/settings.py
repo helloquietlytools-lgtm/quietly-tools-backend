b4826476e7317c9a7092acb64d1fe3d69fdf6f9f
rohituxuistudio <Rohit@uxuistudio.in>
2025-11-07 13:04:20 +0530

    bhb

--- quietly_proj/settings.py
+++ quietly_proj/settings.py
@@ -129,16 +129,16 @@ WSGI_APPLICATION = 'quietly_proj.wsgi.application'
 #     )
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 
 # DATABASES = {
 #     'default': dj_database_url.config(
